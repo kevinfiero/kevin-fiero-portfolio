95c40fdd3f898575c3e4d6d7aa7f9a132318969f
kevinfiero <kjf46@cornell.edu>
2021-01-28 12:02:37 -0800

    refactor projects section

--- src/index.js
+++ src/index.js
@@ -10,9 +10,8 @@ import llamasoftImg from './assets/llamasoft.svg';
 import pepsiImg from './assets/pepsico.svg';
 import linkedIn from './assets/linkedin.png';
 import github from './assets/github.png';
-import ticTacToe from './assets/ticTacToe.svg';
-import zillow from './assets/zillow.svg';
-import gif from './assets/giphy.gif';
+import websiteIcon from './assets/websiteIcon.png';
+import { gifProject, realoProject, ticTacToeProject } from './constants.js';
 
 import "./styles.css";
 
@@ -87,35 +86,59 @@ class FullpageWrapper extends React.Component {
 
               <div className="section column">
 
-              <div className="slide">
-                <h3>Choose Gif</h3>
-                  <div class = 'row'>
-                    <img class = 'slide-img gif-img' src={gif} alt='Gif of Jif'></img>
-                    <div class = 'column'>
-                      <div class = 'section-text left-pad'>Overview: This application allows users to search for their favorite GIFs to easily copy the URL and send to friends. Users can even create an account to favorite GIFs to store for later use. The home page is update in real-time to show the six most popular GIFs and top three trending categories.</div>
-                      <div class = 'section-text left-pad'>Technical Specifications: This project employs a React.js front-end and Node.js back-end. PostgreSQL stores data associated with users' favorites GIFs. The use of media queries allows the application to be mobile friendly. Tests were written using Jest.js to ensure routes are working as expected. This is hosted on GitHub pages.</div>
+                <div className="slide">
+                  <div className='column center'>
+                    <h3>{gifProject.name}</h3>
+                    <img class = 'project-img' src={gifProject.img} alt={gifProject.imgAltText}></img>
+                    <h5>Overview</h5>
+                    <div class = 'section-text'>{gifProject.overview}</div>
+                    <h5>Technical Specifications</h5>
+                    <div class = 'section-text'>{gifProject.technical}</div> 
+                    <div class = 'row project-icon-group'>
+                      <a href = {gifProject.websiteUrl} target = '_blank' rel='noreferrer'>
+                        <img src={websiteIcon} alt='Website Icon'/>
+                      </a>
+                      <a href = {gifProject.githubUrl} target = '_blank' rel='noreferrer'>
+                        <img src={github} alt='Github Icon'/>
+                      </a>
                     </div>
                   </div>
                 </div>
 
                 <div className="slide">
-                <h3>Realo</h3>
-                  <div class = 'row'>
-                    <img class = 'slide-img realo-img' src={zillow} alt='Zillow'></img>
-                      <div class = 'column'>
-                        <div class = 'section-text left-pad'>Overview: This application allows users to sign up for a text messaging service that alerts them as soon as a new property listing is on the market in the city of Portland. Users can input their preferences such as price range, square feet, and number of rooms. Multiple alerts can be set for different range of preferences.</div>
-                        <div class = 'section-text left-pad'>Technical Specifications: This project runs on Node.js and is hosted on Heroku. The front-end is created using Pug.js and is rendered directly on the Heroku dyno. Users have their passwords encrypted using Bcrypt authentication which keeps the application secure. The heart of the application uses Cheerio.js to pull down the HTML off the Zillow web page and jQuery to extract the desired elements. A throttle was used to slow down the request to the web page. The listings, filters, and user information is stored in a PostgreSQL database. Node Mailer automates the sending of text messages via Google's SMTP client.</div> 
-                      </div>
+                  <div className='column center'>
+                    <h3>{realoProject.name}</h3>
+                    <img class = 'project-img' src={realoProject.img} alt={realoProject.imgAltText}></img>
+                    <h5>Overview</h5>
+                    <div class = 'section-text'>{realoProject.overview}</div>
+                    <h5>Technical Specifications</h5>
+                    <div class = 'section-text'>{realoProject.technical}</div> 
+                    <div class = 'row project-icon-group'>
+                      <a href = {realoProject.websiteUrl} target = '_blank' rel='noreferrer'>
+                        <img src={websiteIcon} alt='Website Icon'/>
+                      </a>
+                      <a href = {realoProject.githubUrl} target = '_blank' rel='noreferrer'>
+                        <img src={github} alt='Github Icon'/>
+                      </a>
+                    </div>
                   </div>
                 </div>
 
                 <div className="slide">
-                <h3>Tic-Tac-Toe</h3>
-                  <div class = 'row'>
-                    <img class = 'slide-img tic-tac-img' src={ticTacToe} alt='Tic-Tac-Toe Board'></img>
-                    <div class = 'column'>
-                      <div class = 'section-text left-pad'>Overview: This application allows users to play a virtual game of tic-tac-toe against the computer. The user can choose the difficulty and color of their piece. The overall score is kept track and results can be viewed in a graph.</div>
-                      <div class = 'section-text left-pad'>Technical Specifications: This project uses vanilla JavaScript, HTML, and CSS. The user information such as preferred color, name, difficulty, and score are stored in local storage. Canvas is used to plot the data from local storage once the scoreboard is populated. The competitive game mode algorithm was written by myself and mirrors how I would approach playing a game in real life. Lastly, the colors of the pieces and the line indicating a win are constructed from manipulating the color and rotation of SVG files. The front-end is hosted on Netlify and the back-end is hosted on Heroku.</div>
+                  <div className='column center'>
+                    <h3>{ticTacToeProject.name}</h3>
+                    <img class = 'project-img' src={ticTacToeProject.img} alt={ticTacToeProject.imgAltText}></img>
+                    <h5>Overview</h5>
+                    <div class = 'section-text'>{ticTacToeProject.overview}</div>
+                    <h5>Technical Specifications</h5>
+                    <div class = 'section-text'>{ticTacToeProject.technical}</div> 
+                    <div class = 'row project-icon-group'>
+                      <a href = {ticTacToeProject.websiteUrl} target = '_blank' rel='noreferrer'>
+                        <img src={websiteIcon} alt='Website Icon'/>
+                      </a>
+                      <a href = {ticTacToeProject.githubUrl} target = '_blank' rel='noreferrer'>
+                        <img src={github} alt='Github Icon'/>
+                      </a>
                     </div>
                   </div>
                 </div>
